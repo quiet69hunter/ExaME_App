a677bf1238e17cd45763811fd044d7ca377aede2
AnetaKopera <aneta.kopera11@gmail.com>
2019-06-02 23:49:06 +0200

    Toasty i zabezpieczenia w generowaniu nowego klucza dla testu

--- src/ExaME_Package/Lecturer_ChangeKey.java
+++ src/ExaME_Package/Lecturer_ChangeKey.java
@@ -11,9 +11,20 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Lecturer_ChangeKey {
 
+    //  Sprawdza czy podany String może być sparsowany do Inta
+    private Boolean validateNumberInput(String stringToParse )
+    {
+        String numberRegex = "\\d+";
+        Pattern p = Pattern.compile(numberRegex);
+
+        Matcher m = p.matcher(stringToParse);
+        return m.matches();
+    }
 
     public Scene getLecturer_ChangeKey() throws ClassNotFoundException {
         //  GRID
@@ -42,18 +53,60 @@ public class Lecturer_ChangeKey {
             public void handle(ActionEvent event)
             {
 
-                long Key=System.currentTimeMillis()/1000;
-                Input_T.setText("");
-                DataBaseManager dataBaseManager = null;
-                try {
-                    dataBaseManager = new DataBaseManager();
-                } catch (ClassNotFoundException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
+              if (Input_T.getText().isEmpty())
+                {
+                    Toast.makeToast("INPUT AREA IS EMPTY");
                 }
-                String sql = "UPDATE `test` SET `accessKey`="+Key+"  WHERE id=" + Input_T.getText()  + ";";
-                dataBaseManager.sendQuery_SET(sql);
+              else if(!validateNumberInput(Input_T.getText()))
+              {
+                  Toast.makeToast("INPUT IS NOT A NUMBER");
+              }
+              else {
+
+
+                  long Key = System.currentTimeMillis() / 1000;
+                  DataBaseManager dataBaseManager = null;
+                  try {
+                      dataBaseManager = new DataBaseManager();
+                  } catch (ClassNotFoundException e) {
+                      e.printStackTrace();
+                  } catch (IOException e) {
+                      e.printStackTrace();
+                  }
+
+                  String sql2 = "SELECT id FROM test WHERE idLecturer="+ StartingPoint_Main.globalUser.ID.toString() + ";";
+                  dataBaseManager.sendQuery_GET(sql2);
+
+
+
+
+                  for(int i=0; i < dataBaseManager.resultList.size(); ++i)
+                  {
+
+                      System.out.println(dataBaseManager.resultList.get(i).get("id").toString());
+                      System.out.println(Input_T.getText());
+
+                      if((dataBaseManager.resultList.get(i).get("id").toString().equals(Input_T.getText())))
+                      {
+                          String sql = "UPDATE `test` SET `accessKey`=" + Key + "  WHERE id=" + Input_T.getText() + ";";
+                          dataBaseManager.sendQuery_SET(sql);
+                          Toast.makeToast("KEY WAS SUCCESFULLY CHANGED");
+                          break;
+                      }
+                      else
+                      {
+                          Toast.makeToast("DOESN'T EXIST TEST WITH THIS ID");
+                      }
+
+                  }
+
+                 // if(dataBaseManager.resultList.size()<=0)
+                 // {
+                  //    Toast.makeToast("DOESN'T EXIST TEST WITH THIS ID");
+                 // }
+              }
+
+                Input_T.setText("");
             }
         });
 
